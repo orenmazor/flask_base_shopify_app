1eee42955eeaf4b20c01a239f75c95d5a716a039
Oren Mazor <oren.mazor@protonmail.com>
2019-02-25 20:19:47 -0500

    grab storefront token too

--- app.py
+++ app.py
@@ -1,32 +1,87 @@
-from faker import Faker
-from flask import Flask
+import shopify
+import json
+import os
+
+from flask import Flask, redirect, url_for, render_template, request, session
+from flask_login import LoginManager, login_required, login_user, current_user, UserMixin
 from random import randint
 from zappa.async import task
+from uuid import uuid4
+from wtforms import Form, StringField, validators
 
 from graphql import create_customer_on_shopify, create_order_for_customer_on_shopify
 
 app = Flask(__name__)
+app.secret_key = uuid4().hex
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin):
+    def __init__(self, token, store_url, storefront_token):
+        # gross but i dont care here
+        # if you want to make this not gross, set up proper user objects
+        self.id = json.dumps({"token": token, "store_url": store_url, "storefront_token": storefront_token})
+        self.token = token
+        self.store_url = store_url
+        self.storefront_token = storefront_token
+
+@login_manager.user_loader
+def user_loader(id):
+    return User(**json.loads(id))
 
 @task
 @app.route("/create_customer")
-def create_customer(access_token, store_url, customer_count=20):
-    print("creating {} customers for {}".format(customer_count, store_url))
+def create_customer(access_token, store_url):
+    print("creating customers for {}".format(store_url))
 
-    fake = Faker()
-
-    new_customer_data = {"email": fake.email(),
-                         "firstName": fake.first_name(),
-                         "lastName": fake.last_name()}
-
-    new_customer = create_customer_on_shopify(access_token, store_url, data=new_customer_data)
+    new_customer = create_customer_on_shopify(access_token, store_url)
+    if new_customer["data"]["customerCreate"]["customer"]:
+        new_customer = new_customer["data"]["customerCreate"]["customer"]
 
     order_count = randint(0, 4)
-    for order_idx in xrange(order_count):
-        create_order_for_customer_on_shopify(new_customer["id"])
+    for order_idx in range(order_count):
+        create_order_for_customer_on_shopify(access_token, store_url, new_customer)
 
     print("done!")
 
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect(url_for('login'))
+
+@app.route("/oauth_callback")
+def oauth_callback():
+    shopify.Session.setup(api_key=os.environ.get("API_KEY"), secret=os.environ.get("SHARED_SECRET"))
+    shopify_session = shopify.Session(request.args['shop'])
+
+    token = shopify_session.request_token(request.args)
+    with shopify.Session.temp(request.args['shop'], token):
+        storefront_token = shopify.StorefrontAccessToken.create({"title": "0xDEADBEEF"})
+    u = User(token=token, store_url=request.args['shop'], storefront_token=storefront_token.access_token)
+    login_user(u)
+
+    return redirect(url_for("index"))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    class ShopifyShopForm(Form):
+        store_name = StringField('shopify_store_name', [validators.Length(min=4, max=1024)])
+    
+    form = ShopifyShopForm(request.form)
+
+    if form.validate():
+        shopify.Session.setup(api_key=os.environ.get("API_KEY"), secret=os.environ.get("SHARED_SECRET"))
+        shopify_session = shopify.Session(request.form['store_name'])
+
+        scope=["write_customers", "write_orders", "unauthenticated_write_checkouts"]
+        return redirect(shopify_session.create_permission_url(scope, url_for("oauth_callback", _external=True, _scheme=app.config.get("PREFERRED_URL_SCHEME"))))
+
+    return render_template("shop_login.html")
+
 @app.route("/")
+@login_required
 def index():
-    create_customer("39da5abe709be02c08da67663398ce91", "whydontyoujust.myshopify.com")
+    for idx in range(10):
+        # zappa will make this run async in lambdas <3
+        create_customer(current_user.token, current_user.store_url)
+
     return "Hello World!"
--- graphql.py
+++ graphql.py
@@ -1,5 +1,7 @@
 import requests
 
+from faker import Faker
+
 def _make_request(access_token, store_url, query):
     headers = {"X-Shopify-Access-Token": access_token,
                "Content-Type": "application/graphql"}
@@ -9,23 +11,31 @@ def _make_request(access_token, store_url, query):
                              headers=headers,
                              data=query)
 
-    import pdb; pdb.set_trace()
     response.raise_for_status()
+    import pdb;pdb.set_trace()
+    print("query to shopify complete. throttle info: {}".format(response.json()["extensions"]["cost"]))
 
     return response.json()
 
-def create_customer_on_shopify(access_token, store_url, data):
+def create_customer_on_shopify(access_token, store_url):
+    # TODO: for my uses all I need is the email, but this could be expanded to have more
+
+    faker = Faker()
+
     full_query = "".join(["mutation {",
-                         "customerCreate(input: {{email: \"{}\"}}) {{".format("asdf@google.com"),
-                         "customer { id email }",
+                         "customerCreate(input: {{email: \"{}\"}}) {{".format(faker.email()),
+                         "customer { email }",
                          "userErrors { field message } }}"])
 
     customer = _make_request(access_token, store_url, full_query)
 
     return customer
 
-def create_order_for_customer_on_shopify(id, access_token, store_url, data):
-    order = _make_request(access_token, store_url, data)
-
+def create_order_for_customer_on_shopify(access_token, store_url, customer_obj):
     import pdb; pdb.set_trace()
-    pass
+    full_query = "".join(["mutation {",
+                         "checkoutCreate(input: {{email: \"{}\", lineItems: [{{quantity: 1, variantId: 32146881610}}]}}) {{".format(customer_obj["email"]),
+                         "checkout { id }",
+                         "userErrors { field message } }}"])
+
+    order = _make_request(access_token, store_url, full_query)
